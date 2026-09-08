39f0f26ee886c8d4feea2150680a7089ced7fb7c
Daniel Parraguez <daniel.parraguez04@inacapmail.cl>
2026-09-07 22:51:13 -0300

    Set up Django project scaffolding, base template, and environment config

---
+++ safenotes/urls.py
@@ -0,0 +1,12 @@
+"""
+URL configuration for safenotes project.
+"""
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+]
